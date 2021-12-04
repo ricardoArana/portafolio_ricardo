471f3498c382e4943c05b76fccf68bb09d02eea6
ricardoArana <ricardo.romero@iesdonana.org>
2021-12-04 18:42:48 +0100

    Solo queda validar formulario

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-const bur = document.getElementById("burger_img");
-let menu = document.getElementById("burger");
-document.getElementById("burger_img").addEventListener("click", cambiar)
-function cambiar()
-{
-    bur.setAttribute("style", "none")
-    document.getElementById("burger").style.display = "flex";
-}
+
+setInterval(() => {
+    var img = document.getElementById("img");
+    var bur = document.getElementById("burger_img");
+    var menu = document.getElementById("burger");
+
+    if (img.getAttribute("src").match("burger")){
+        bur.addEventListener("click", abrir);
+        function abrir()
+        {
+            img.setAttribute("src", "/img/cerrar.png");
+            menu.style.display = "flex";
+            bur.setAttribute("id", "cerrar");
+        }
+    }
+    else{
+        bur = document.getElementById("cerrar");
+        bur.addEventListener("click", cerrar);
+        function cerrar() 
+        {
+            img.setAttribute("src", "/img/burger.png");
+            menu.style.display = "none";
+            bur.setAttribute("id", "burger_img");
+        }
+    }}, 100)
