79947f99fd1d3937bd011d96f2cc17c64fbc8afc
ricardoArana <ricardo.romero@iesdonana.org>
2021-12-05 18:39:49 +0100

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-setInterval(() => {
+setInterval(() => { //Vamos comprobando si el menú está abierto o cerrado, para saber que debe hacer el click.
     var img = document.getElementById("img");
     var bur = document.getElementById("burger_img");
     var menu = document.getElementById("burger");
@@ -23,3 +23,22 @@ setInterval(() => {
             bur.setAttribute("id", "burger_img");
         }
     }}, 100)
+
+    //Formulario
+    let env = document.getElementById("enviar")
+    let n = document.forms[0].nombre
+    let e = document.forms[0].email
+    n.addEventListener("focusout", validar);
+    e.addEventListener("focusout", validar);
+
+    function validar(){ //Podemos ver que de esta forma no se puede acceder al text area hasta que no estén rellenos los otros dos campos
+        if(!n.checkValidity())
+        {
+            n.reportValidity()
+        }
+        else if(!e.checkValidity())
+        {
+            e.reportValidity()
+        }
+}
+    
